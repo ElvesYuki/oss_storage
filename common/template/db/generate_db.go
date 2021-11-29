c26b705e68a89daaf2940c71668374f8b32cf910
luohuan <1026770043@qq.com>
2021-11-29 18:33:45 +0800

    temp

---
+++ common/template/db/generate_db.go
@@ -0,0 +1,16 @@
+package common
+
+type tableCol struct {
+}
+
+func GenerateDbModel(dbName string) {
+	//tmpl, err := template.ParseFiles("./common/template/db/template_mode.tmpl")
+	//if err != nil {
+	//	return
+	//}
+	//
+	//
+	//
+	//sqlStr := `select * from information_schema.COLUMNS where TABLE_SCHEMA = 'oss_storage' and TABLE_NAME = ?`
+
+}
